f01c93775157171fe582a00b775137e5fe3b2a2e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-02 16:49:18 +0900

    fix : add secured in text field

--- Projects/UserInterface/DesignSystem/Sources/InfinityTextField.swift
+++ Projects/UserInterface/DesignSystem/Sources/InfinityTextField.swift
@@ -12,20 +12,28 @@ public struct InfinityTextField: View {
     
     var hint: String
     @Binding var text: String
+    var isSecured: Bool
     
-    public init(_ hint: String, text: Binding<String>) {
+    public init(_ hint: String, text: Binding<String>, isSecured: Bool = false) {
         self.hint = hint
         self._text = text
+        self.isSecured = isSecured
     }
     
     public var body: some View {
-    
-        TextField(hint, text: $text)
-            .padding(15)
-            .overlay {
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(lineWidth: 1.0)
-                    .foregroundStyle(.gray.opacity(0.3))
+        
+        Group {
+            if isSecured {
+                SecureField(hint, text: $text)
+            } else {
+                TextField(hint, text: $text)
             }
+        }
+        .padding(15)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(lineWidth: 1.0)
+                .foregroundStyle(.gray.opacity(0.3))
+        }
     }
 }
