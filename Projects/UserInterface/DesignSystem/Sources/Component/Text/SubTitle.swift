4543524a7d23cf8d4faf0f9da71a7e209f202d29
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-06 14:02:11 +0900

    feat : create sub title component

---
+++ Projects/UserInterface/DesignSystem/Sources/Component/Text/SubTitle.swift
@@ -0,0 +1,29 @@
+//
+//  SubTitle.swift
+//  DesignSystem
+//
+//  Created by dgsw8th71 on 4/6/24.
+//  Copyright © 2024 molohala. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SubTitle: View {
+    
+    private let text: String
+    
+    public init(
+        _ text: String
+    ) {
+        self.text = text
+    }
+    
+    public var body: some View {
+        HStack {
+            Text(text)
+                .font(.title2)
+                .fontWeight(.semibold)
+            Spacer()
+        }
+    }
+}
