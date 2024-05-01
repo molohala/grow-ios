6c03ccf3c296536ffa9334f62abb55d50a96ec63
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-01 17:19:21 +0900

    feat : impl rank datasource

--- Projects/Service/RankService/Interface/DataSource/RankDataSource.swift
+++ Projects/Service/RankService/Interface/DataSource/RankDataSource.swift
@@ -2,5 +2,8 @@ public protocol RankDataSource {
     func getWeekGithubRank() async throws -> [Rank]
     func getTotalGithubRank() async throws -> [Rank]
     func getTodayGithubRank() async throws -> [Rank]
+    
+    func getWeekSolvedacRank() async throws -> [Rank]
+    func getTotalSolvedacRank() async throws -> [Rank]
+    func getTodaySolvedacRank() async throws -> [Rank]
 }
-
--- Projects/Service/RankService/Sources/DataSource/RankDataSourceImpl.swift
+++ Projects/Service/RankService/Sources/DataSource/RankDataSourceImpl.swift
@@ -2,18 +2,32 @@ import BaseService
 import RankServiceInterface
 
 class RankDataSourceImpl: BaseRemoteDataSource<RankDataSourceImpl>, RankDataSource {
-    
-    private let path = "rank/github"
+    private let githubPath = "rank/github"
+    private let solvedacPath = "rank/solvedac"
     
     func getWeekGithubRank() async throws -> [Rank] {
-        try await requestGet(url: "\(Infinity.baseUrl)/\(path)/week", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+        try await requestGet(url: "\(Infinity.baseUrl)/\(githubPath)/week", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
     }
     
     func getTotalGithubRank() async throws -> [Rank] {
-        try await requestGet(url: "\(Infinity.baseUrl)/\(path)/total", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+        try await requestGet(url: "\(Infinity.baseUrl)/\(githubPath)/total", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
     }
     
     func getTodayGithubRank() async throws -> [Rank] {
-        try await requestGet(url: "\(Infinity.baseUrl)/\(path)/today", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+        try await requestGet(url: "\(Infinity.baseUrl)/\(githubPath)/today", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
     }
+    
+    func getWeekSolvedacRank() async throws -> [Rank] {
+        try await requestGet(url: "\(Infinity.baseUrl)/\(solvedacPath)/week", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+    }
+    
+    func getTotalSolvedacRank() async throws -> [Rank] {
+        try await requestGet(url: "\(Infinity.baseUrl)/\(solvedacPath)/total", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+    }
+    
+    func getTodaySolvedacRank() async throws -> [Rank] {
+        try await requestGet(url: "\(Infinity.baseUrl)/\(solvedacPath)/today", res: BaseResponse<[RankResponse]>.self).data.map { $0.toDomain() }
+    }
+    
+    
 }
