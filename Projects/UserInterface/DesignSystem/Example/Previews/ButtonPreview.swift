935290d0c0f15d9fca6a5224a9d998b7bcc1e67f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-05 23:48:05 +0900

    feat : GrowTabButton 추가

--- Projects/UserInterface/DesignSystem/Example/Previews/ButtonPreview.swift
+++ Projects/UserInterface/DesignSystem/Example/Previews/ButtonPreview.swift
@@ -6,22 +6,23 @@ struct ButtonPreview: View {
         ScrollView {
             VStack {
                 ForEach(ButtonType.allCases, id: \.self) {
-                    GrowButton("완료", type: $0, leadingIcon: .check) {
-                        
-                    }
+                    GrowButton("완료", type: $0, leadingIcon: .check) {}
                     GrowButton("시작하기", type: $0) {
                         try? await Task.sleep(for: .seconds(1))
                     }
                     GrowButton("시작하기", type: $0, isEnabled: false) {}
                     
-                    GrowTextButton("완료", type: $0, leadingIcon: .check) {
-                        
-                    }
+                    GrowTextButton("완료", type: $0, leadingIcon: .check) {}
                     GrowTextButton("시작하기", type: $0) {
                         try? await Task.sleep(for: .seconds(1))
                     }
                     GrowTextButton("시작하기", type: $0, isEnabled: false) {}
                 }
+                GrowTabButton("완료", leadingIcon: .check) {}
+                GrowTabButton("시작하기") {
+                    try? await Task.sleep(for: .seconds(1))
+                }
+                GrowTabButton("시작하기", isSelected: false) {}
             }
         }
         .growTopBar("") {}
