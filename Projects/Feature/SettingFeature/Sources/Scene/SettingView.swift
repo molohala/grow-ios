e2b7c3bf12b4296e8fcefbbcaf476f5ad6f0a1c6
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-10 12:23:52 +0900

    fix : add ScrollView in settingview

--- Projects/Feature/SettingFeature/Sources/Scene/SettingView.swift
+++ Projects/Feature/SettingFeature/Sources/Scene/SettingView.swift
@@ -20,89 +20,91 @@ public struct SettingView: View {
     }
     
     public var body: some View {
-        LazyVStack(spacing: 20) {
-            VStack(spacing: 12) {
-                if case .success(let profile) = appState.profile {
-                    GrowSettingCell(
-                        label: "프로필 설정",
-                        leftIcon: .person,
-                        description: profile.name
-                    ) {
-                        router.navigate(to: SettingDestination.profileEdit)
-                    }
-                    GrowSettingCell(
-                        label: "Github 설정",
-                        leftIcon: .github,
-                        description: profile.githubId
-                    ) {
-                        router.navigate(to: SettingDestination.githubSetting)
-                    }
-                    let baekjoon = profile.socialAccounts.first(where: { $0.socialType == .SOLVED_AC })
-                    GrowSettingCell(
-                        label: "백준 설정",
-                        leftIcon: .baekjoon,
-                        description: profile.baekjoonId
-                    ) {
-                        router.navigate(to: SettingDestination.baekjoonSetting)
+        ScrollView {
+            LazyVStack(spacing: 20) {
+                VStack(spacing: 12) {
+                    if case .success(let profile) = appState.profile {
+                        GrowSettingCell(
+                            label: "프로필 설정",
+                            leftIcon: .person,
+                            description: profile.name
+                        ) {
+                            router.navigate(to: SettingDestination.profileEdit)
+                        }
+                        GrowSettingCell(
+                            label: "Github 설정",
+                            leftIcon: .github,
+                            description: profile.githubId
+                        ) {
+                            router.navigate(to: SettingDestination.githubSetting)
+                        }
+                        let baekjoon = profile.socialAccounts.first(where: { $0.socialType == .SOLVED_AC })
+                        GrowSettingCell(
+                            label: "백준 설정",
+                            leftIcon: .baekjoon,
+                            description: profile.baekjoonId
+                        ) {
+                            router.navigate(to: SettingDestination.baekjoonSetting)
+                        }
                     }
                 }
+                GrowDivider()
+                VStack(spacing: 12) {
+                    GrowSettingCell(
+                        label: "알림 허용",
+                        leftIcon: .notification,
+                        content: {
+                            GrowToggle(isOn: .constant(true))
+                        }
+                    )
+                    GrowSettingCell(
+                        label: "다크모드",
+                        leftIcon: .moon,
+                        content: {
+                            GrowToggle(isOn: .init(get: {
+                                colorProvider.isDarkTheme
+                            }, set: { bool in
+                                colorProvider.isDarkTheme = bool
+                            }))
+                        }
+                    )
+                }
+                GrowDivider()
+                VStack(spacing: 12) {
+                    GrowSettingCell(
+                        label: "로그아웃",
+                        action: {
+                            appState.accessToken = ""
+                            appState.refreshToken = ""
+                        }
+                    )
+                    GrowSettingCell(
+                        label: "회원탈퇴",
+                        labelColor: .textWarning,
+                        action: {
+                            // action
+                        }
+                    )
+                }
+                VStack(spacing: 16) {
+                    Text("버전 - \(version ?? "")")
+                        .growFont(.labelM)
+                        .growColor(.textAlt)
+                    Text("개인정보 이용 약관")
+                        .growFont(.labelM)
+                        .growColor(.textAlt)
+                        .underline()
+                    Text("서비스 정책")
+                        .growFont(.labelM)
+                        .growColor(.textAlt)
+                        .underline()
+                }
+                .padding(.vertical, 8)
+                Spacer()
             }
-            GrowDivider()
-            VStack(spacing: 12) {
-                GrowSettingCell(
-                    label: "알림 허용",
-                    leftIcon: .notification,
-                    content: {
-                        GrowToggle(isOn: .constant(true))
-                    }
-                )
-                GrowSettingCell(
-                    label: "다크모드",
-                    leftIcon: .moon,
-                    content: {
-                        GrowToggle(isOn: .init(get: {
-                            colorProvider.isDarkTheme
-                        }, set: { bool in
-                            colorProvider.isDarkTheme = bool
-                        }))
-                    }
-                )
-            }
-            GrowDivider()
-            VStack(spacing: 12) {
-                GrowSettingCell(
-                    label: "로그아웃",
-                    action: {
-                        appState.accessToken = ""
-                        appState.refreshToken = ""
-                    }
-                )
-                GrowSettingCell(
-                    label: "회원탈퇴",
-                    labelColor: .textWarning,
-                    action: {
-                        // action
-                    }
-                )
-            }
-            VStack(spacing: 16) {
-                Text("버전 - \(version ?? "")")
-                    .growFont(.labelM)
-                    .growColor(.textAlt)
-                Text("개인정보 이용 약관")
-                    .growFont(.labelM)
-                    .growColor(.textAlt)
-                    .underline()
-                Text("서비스 정책")
-                    .growFont(.labelM)
-                    .growColor(.textAlt)
-                    .underline()
-            }
-            .padding(.vertical, 8)
-            Spacer()
+            .padding(.top, 12)
+            .padding(.horizontal, 16)
         }
-        .padding(.top, 12)
-        .padding(.horizontal, 16)
         .growTopBar("설정", background: .backgroundAlt) {
             router.popToStack()
         }
