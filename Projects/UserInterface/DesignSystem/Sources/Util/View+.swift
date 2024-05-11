8acbfef785fc5a7a08f357bfaf8fa2eba2e1324e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-11 11:02:39 +0900

    fix : press animation

--- Projects/UserInterface/DesignSystem/Sources/Util/View+.swift
+++ Projects/UserInterface/DesignSystem/Sources/Util/View+.swift
@@ -13,7 +13,7 @@ public extension View {
         self
             .opacity(isPressed ? 0.64 : 1)
             .scaleEffect(isPressed ? 0.97 : 1)
-            .animation(.easeOut, value: 1)
+            .animation(.timingCurve(0.11, 0.9, 0.265, 0.945, duration: 0.3), value: isPressed)
             .disabled(isPressed)
     }
     
